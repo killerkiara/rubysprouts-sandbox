98b02ad81871b9af1fbbeb24b40d034be35b7b3c
killerkiara <chiara.sarta@gmail.com>
2017-01-11 17:45:52 +0100

    Group exercises into sandbox repo

---
+++ task.rb
@@ -0,0 +1,11 @@
+puts "TASK"
+# This script will output a poem.
+# Here you assign the argument to a variable
+
+puts "\"I want a dyke for president"
+# And here you can print the value of your argument using your variable
+puts ARGV[0]
+puts 'I want a Black woman for president'
+puts "I want someone with bad teeth...\""
+puts 'Zoe Leonard' + ARGV[1]
+puts '1992'
